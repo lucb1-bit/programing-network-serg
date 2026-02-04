1455290ef3a031b001e51adaeb5d1ac91d5d31e4
lucb1-bit <lucerocardozoburgos1@gmail.com>
2026-02-04 18:30:15 +0100

    This is the Ex-3 sum20.py

---
+++ S02/fiboN.py
@@ -0,0 +1,14 @@
+fibo_list = ()
+for i in range (0,11):
+    n1 = 0
+    n2 = 1
+    res = n1 + n2
+    res1 =
+    if res == 0:
+        fibo_list.append(0)
+    elif res == 1:
+        fibo_list.append(1)
+    else:
+        fibo_list.append(1)
+
+    print (res)
