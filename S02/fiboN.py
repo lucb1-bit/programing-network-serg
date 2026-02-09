b4bcad2c9c6076da3aaf442ec41b246bd5d36a04
lucb1-bit <lucerocardozoburgos1@gmail.com>
2026-02-09 18:14:07 +0100

    This is the Ex-3 sum20.py

--- S02/fiboN.py
+++ S02/fiboN.py
@@ -1,14 +1,11 @@
-fibo_list = ()
+fibo_list = []
 for i in range (0,11):
-    n1 = 0
-    n2 = 1
-    res = n1 + n2
-    res1 =
-    if res == 0:
+    n1= (len(fibo_list)-1)
+    n2= (len(fibo_list)-2)
+    if i == 0:
         fibo_list.append(0)
-    elif res == 1:
+    elif i == 1:
         fibo_list.append(1)
     else:
-        fibo_list.append(1)
-
-    print (res)
+        fibo_list.append(fibo_list[n1] + fibo_list[n2] )
+print (fibo_list)
