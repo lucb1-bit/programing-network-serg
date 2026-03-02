4bdfab14a7b47b8babbc9b6386c57221ce67dfcb
lucb1-bit <lucerocardozoburgos1@gmail.com>
2026-03-02 20:18:26 +0100

    S07.2

--- P01/Seq1.py
+++ P01/Seq1.py
@@ -29,4 +29,28 @@ class Seq:
                 return False
         return True
 
-def count_base(self, base)
+    def count_base(self,bases):
+        A, C, T, G = 0,0,0,0
+        if bases == "NULL" or bases == "Invalid Sequence":
+            A,C,T,G = 0,0,0,0
+        else:
+            for b in self.bases:
+                if b == "A":
+                    A += 1
+                elif b == "C":
+                    C += 1
+                elif b == "T":
+                    T += 1
+                elif b == "G":
+                    G += 1
+        return "A:", A, "C:", C, "T:", T, "G:", G
+
+    def count(self):
+        bases_dict = {'A':0, 'C':0, 'T':0, 'G':0}
+        if self.bases == "NULL" or self.bases == "Invalid Sequence":
+            bases_dict = {'A':0, 'C':0, 'T':0, 'G':0}
+        else:
+            for b in self.bases:
+                if b in bases_dict:
+                    bases_dict[b] += 1
+        return bases_dict
---
+++ P01/e6.py
@@ -0,0 +1,10 @@
+from Seq1 import Seq
+
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("PPP")
+
+print(f"Sequence 1: (Length: {s1.len()}) {s1} \n {s1.count()}")
+print(f"Sequence 2: (Length: {s2.len()}) {s2} \n {s2.count()}")
+print(f"Sequence 3: (Length: {s3.len()}) {s3} \n {s3.count()}")
+
