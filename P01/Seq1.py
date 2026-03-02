7361e5d7482b274fe0b5c83c1d582ef411a48dcd
lucb1-bit <lucerocardozoburgos1@gmail.com>
2026-03-02 18:01:18 +0100

    S07

--- P01/Seq1.py
+++ P01/Seq1.py
@@ -1,32 +1,32 @@
 class Seq:
 
-    def __init__(self, bases):
-        if self.is_valid(bases) == True:
-            self.bases = bases
-            print("New sequence created!")
+    def __init__(self, bases=None):
+        if bases == None:
+            self.bases = "NULL"
+            print(" NULL sequence created!")
         else:
-            self.bases = "ERROR"
-            print("INCORRECT Sequence detected")
+            isvalid = self.is_valid(bases)
+            if isvalid == True:
+                self.bases = bases
+                print("New sequence created!")
+            elif isvalid == False:
+                self.bases = "Invalid Sequence"
+                print("INCORRECT Sequence detected")
 
     def __str__(self):
         return self.bases
 
     def len(self):
-        return len(self.bases)
+        if (self.bases == "NULL") or (self.bases == "Invalid Sequence"):
+            return 0
+        else:
+            return len(self.bases)
 
     def is_valid(self, bases):
         valid_bases = {'A', 'C', 'G', 'T'}
-        result = 0
         for b in bases:
-            if b in valid_bases:
-                result = 0
-            else:
-                result +=1
+            if b not in valid_bases:
+                return False
+        return True
 
-        if result == 0:
-            return True
-        else:
-            return False
-
-
-
+def count_base(self, base)
---
+++ P01/e3.py
@@ -0,0 +1,9 @@
+from Seq1 import Seq
+
+s1 = Seq()
+s2 = Seq("AAA")
+s3 = Seq("PPP")
+
+print(f"Sequence 1: {s1}")
+print(f"Sequence 2: {s2}")
+print(f"Sequence 3: {s3}")
