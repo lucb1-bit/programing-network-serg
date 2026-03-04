c85ac819f37c1d0d1a44645ffd4a6c5422462e85
lucb1-bit <lucerocardozoburgos1@gmail.com>
2026-03-04 19:19:07 +0100

    s08

---
+++ S08/e3.py
@@ -0,0 +1,22 @@
+import socket
+
+# SERVER IP, PORT
+PORT = 8081
+IP = "192.168.124.179" # depends on the computer the server is running
+
+while True:
+  # -- Ask the user for the message
+  msg = input("escribe:")
+
+  # -- Create the socket
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  # -- Establish the connection to the Server
+  s.connect((IP, PORT))
+
+  # -- Send the user message
+  s.send(str.encode(msg))
+
+  # -- Close the socket
+  s.close()
+
