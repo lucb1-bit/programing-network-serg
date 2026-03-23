e943b01f4ec5b4d2b43d66a90ae54471cf57ccbd
lucb1-bit <lucerocardozoburgos1@gmail.com>
2026-03-23 19:57:00 +0100

    p04

--- S12/webserver-e4.py
+++ S12/webserver-e4.py
@@ -6,13 +6,47 @@ import termcolor
 IP = "127.0.0.1"
 PORT = 8080
 
+
 def process_client(s):
     # -- Receive the request message
     req_raw = s.recv(2000)
     req = req_raw.decode()
 
     print("Message FROM CLIENT: ")
-    termcolor.cprint(req, "green")
+
+    # -- Split the request messages into lines
+    lines = req.split('\n')
+
+    # -- The request line is the first
+    req_line = lines[0]
+
+    print("Request line: ", end="")
+    termcolor.cprint(req_line, "green")
+
+    # -- Generate the response message
+    # It has the following lines
+    # Status line
+    # header
+    # blank line
+    # Body (content to send)
+
+    # This new contents are written in HTML language
+    with open("index.html", "r") as f:
+        body = f.read()
+
+    # -- Status line: We respond that everything is ok (200 code)
+    status_line = "HTTP/1.1 200 OK\n"
+
+    # -- Add the Content-Type header
+    header = "Content-Type: text/html\n"
+
+    # -- Add the Content-Length
+    header += f"Content-Length: {len(body)}\n"
+
+    # -- Build the message by joining together all the parts
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
 
 # -------------- MAIN PROGRAM
 # ------ Configure the server
@@ -28,14 +62,13 @@ ls.bind((IP, PORT))
 # -- Become a listening socket
 ls.listen()
 
-print("Echo server configured!")
+print("Green server configured!")
 
 # --- MAIN LOOP
 while True:
     print("Waiting for clients....")
     try:
         (cs, client_ip_port) = ls.accept()
-        print("Connection established!")
     except KeyboardInterrupt:
         print("Server stopped!")
         ls.close()
